3187f88af950c1e01727edbc79e33a5730626aba
Michał <97108402+Berettt@users.noreply.github.com>
2022-08-18 15:25:10 +0200

    16-line code simple autoclicker

---
+++ projects/Project 29v/AutoClicker.py
@@ -0,0 +1,17 @@
+import pyautogui, keyboard, time
+def autoclicker(time_between_clicks=1):
+
+
+  while True:
+    time.sleep(time_between_clicks)
+    pyautogui.click()
+
+    if keyboard.is_pressed('/'):
+      break
+
+if __name__ == '__main__':
+  wait = float(input('Type in time between clicks'))
+  time.sleep(1)
+  print('autoclicker is active!')
+  print('hold "/" to stop')
+  autoclicker(wait)
