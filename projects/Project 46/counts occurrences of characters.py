b8351bfbdd98e18dc88b4707eb585c8d818e2cf4
Michał <97108402+Berettt@users.noreply.github.com>
2022-08-23 11:18:38 +0200

    Small visual update

--- projects/Project 46/counts occurrences of characters.py
+++ projects/Project 46/counts occurrences of characters.py
@@ -3,17 +3,27 @@ class Count_occurrences:
     #input
     def user_input(self)->str:
         self.user = input('Type in a STRING: ')
+        print('Type in a certain character of string ')
+        print('If you want to count all occurrences just press enter ')
+        self.char = input('--> ')
 
 
     #count occurrences of input
     def count(self):
+        self.user_input()
         print('String contains: ')
+
         for i in range(len(self.user)):
-            self.occurrences = self.user.count(self.user[i])
-            
-            print(f'{self.user[i]} = {self.occurrences}')
+            if self.char == '':
+                print(f'{self.user[i]} == {self.user.count(self.user[i])}')
+        print(f'Number of characters: {i+1} ')
+        if self.char != '':
+            print(f"Certain character: '{self.char}' : {self.user.count(self.char)} ")
 
 
-C = Count_occurrences()
-C.user_input()
-C.count()
+def Countoccurrences():
+    C = Count_occurrences()
+    C.count()
+
+if __name__ == '__main__':
+    Countoccurrences()
