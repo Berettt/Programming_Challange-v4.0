1aa41644268f39a56c47efa57eccce24dd65c21d
Michał <97108402+Berettt@users.noreply.github.com>
2022-08-28 14:23:42 +0200

    TowersOfHanoi

--- projects/Project 47/TowersOfHanoi.py
+++ projects/Project 47/TowersOfHanoi.py
@@ -1,16 +1,6 @@
-def Towers():
-
-    hanoi = 4
-
-    a=[i+1 for i in range(hanoi)]
-    a.reverse()
-
-   
-
-
-    print(a)
-
-
-
-Towers()
-
+def Towers(n=4,a=[],b=[],c=[]):
+    if n > 0:
+        Towers(n-1,a,b,c)
+        c.append(b.pop())
+        Towers(n-1,b,c,a)
+Towers(a=[z+1 for z in range(4)])
